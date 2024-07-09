357c22bf74798108f4aa000dbd5cfc584fe41cf0
FelipeNazar <nazarfinv@gmail.com>
2024-07-09 00:47:18 -0400

    Version final clientes

--- src/Pages/GestionCoffeCliente.jsx
+++ src/Pages/GestionCoffeCliente.jsx
@@ -3,8 +3,8 @@ import Navbar from '../Components/Shared/Navbar';
 import Footer from '../Components/Shared/Footer';
 
 const mockCoffees = [
-  { idCoffee: 1, name: 'Coffee #1', description: 'Descripción Coffee #1', price: 4500 },
-  { idCoffee: 2, name: 'Coffee #2', description: 'Descripción Coffee #2', price: 5000 },
+  { username: 'NazarCustomer', email: 'felipe@alu.cl', disable: 0, locked:0  },
+  { username: 'NazarAdmin', email: 'nazarfinv@gmail.com', disable: 0, locked:0 },
   // Puedes agregar más datos aquí
 ];
 
@@ -20,23 +20,24 @@ function GestionCoffeesPage() {
             <table className="min-w-full border">
               <thead>
                 <tr>
-                  <th className="border px-4 py-2">ID</th>
-                  <th className="border px-4 py-2">Nombre</th>
-                  <th className="border px-4 py-2">Descripción</th>
-                  <th className="border px-4 py-2">Precio</th>
+                  <th className="border px-4 py-2">username</th>
+                  <th className="border px-4 py-2">email</th>
+                  <th className="border px-4 py-2">disable</th>
+                  <th className="border px-4 py-2">locked</th>
                   <th className="border px-4 py-2">Acciones</th>
                 </tr>
               </thead>
               <tbody>
                 {mockCoffees.map((coffee) => (
                   <tr key={coffee.idCoffee}>
-                    <td className="border px-4 py-2">{coffee.idCoffee}</td>
-                    <td className="border px-4 py-2">{coffee.name}</td>
-                    <td className="border px-4 py-2">{coffee.description}</td>
-                    <td className="border px-4 py-2">${coffee.price}</td>
+                    
+                    <td className="border px-4 py-2">{coffee.username}</td>
+                    <td className="border px-4 py-2">{coffee.email}</td>
+                    <td className="border px-4 py-2">${coffee.disable}</td>
+                    <td className="border px-4 py-2">${coffee.locked}</td>
                     <td className="border px-4 py-2">
                       <button className="bg-yellow-500 text-white px-2 py-1 rounded mr-2">Editar</button>
-                      <button className="bg-red-500 text-white px-2 py-1 rounded">Eliminar</button>
+                      
                     </td>
                   </tr>
                 ))}
