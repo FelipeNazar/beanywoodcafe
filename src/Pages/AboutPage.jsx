82e6addf93bb5e1892e4e1d4300912abda9c9ac7
FelipeNazar <nazarfinv@gmail.com>
2024-07-08 21:51:00 -0400

    Añadir desarrollador en acerca de y authcontext

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -46,14 +46,12 @@ function AboutPage() {
         </section>
         <section className="container mx-auto text-center mt-12">
           <h2 className="text-3xl font-bold mb-8">Desarrolladores</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-           
+          <div className="flex justify-center">
             <div className="text-center">
-              <img src="/felipenazar.webp" alt="Benjamín Navarrete" className="w-32 h-32 mx-auto mb-4 rounded-full" />
+              <img src="/felipenazar.webp" alt="Felipe Nazar" className="w-32 h-32 mx-auto mb-4 rounded-full" />
               <h3 className="text-xl font-semibold">Felipe Nazar</h3>
               <p className="text-gray-600">Desarrollador Full Stack</p>
             </div>
-            
           </div>
         </section>
       </main>
